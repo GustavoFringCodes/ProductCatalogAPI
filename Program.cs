111e61fa3896a51677fce072e9209fd73c33a4ab
GustavoFringCodes <gustavogusfring123@gmail.com>
2025-06-20 16:08:02 +0400

    Updated Npgsql to 8.0.0

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProductCatalogAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 var builder = WebApplication.CreateBuilder(args);
 
